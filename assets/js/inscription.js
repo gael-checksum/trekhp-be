035c32cfdb2d31fc438e067793274751fba68df1
gael-checksum <gael-checksum@users.noreply.github.com>
2026-04-28 20:48:06 +0000

    deploy: 7872dbd85bccfe6a4d93ee0d85331db471d264a7

--- assets/js/inscription.js
+++ assets/js/inscription.js
@@ -172,7 +172,7 @@ function submitInscription(e) {
   fetch(TREK_CONFIG.apps_script_inscription, {
     method: "POST",
     mode:   "no-cors",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "text/plain;charset=utf-8" },
     body:   JSON.stringify(data)
   })
   .then(() => {
